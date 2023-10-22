12dc783e429970130d3350b0c288b3c640805bfe
takahiro2001kt <67308009+takahiro2001kt@users.noreply.github.com>
2023-10-22 16:48:09 +0900

    change file path

--- dav_to_mp4.py
+++ dav_to_mp4.py
@@ -4,24 +4,23 @@ import time
 
 totalstart = time.time()
 
-# 入力フォルダと出力フォルダを定義
-input_folder = '/Users/takahiro/Desktop/testdata'
-output_folder = '/Users/takahiro/Desktop/savedata'
+# Define input and output folders (must be changed).
+input_folder = 'INPUT FOLDER PATH'
+output_folder = 'OUTPUT FOLDER PATH'
 
-# 出力フォルダが存在しない場合は作成
+# Create output folder if it does not exist
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 
-# 入力フォルダ内の全ファイルを走査
+# Scan all files in the input folder.
 for file in os.listdir(input_folder):
     start = time.time()
     if file.endswith('.dav'):
-        # 入力ファイルと出力ファイルのパスを設定
         input_file = os.path.join(input_folder, file)
         output_file = os.path.join(output_folder, file.replace('.dav', '.mp4'))
 
-        # ffmpeg コマンドを使用してファイルを変換
+        # Convert files using the ffmpeg command
         print(f"{file} is currently under conversion")
         command = ['ffmpeg', '-i', input_file, output_file]
         subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
